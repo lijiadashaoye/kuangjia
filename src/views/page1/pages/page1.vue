e289cd832d89a8f078c68217c2d949229b4334b4
lipeng <15646507519@sina.cn>
2022-02-27 23:17:58 +0800

    f

--- src/views/page1/pages/page1.vue
+++ src/views/page1/pages/page1.vue
@@ -1,22 +1,16 @@
 <template>
-  <div class="page">
-    page1
- </div>
+  <div class="page">page1</div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {};
   },
   created() {},
-  methods: {
-   
-  },
+  methods: {},
 };
 </script>
 
 <style scoped lang='scss'>
-
 </style>
